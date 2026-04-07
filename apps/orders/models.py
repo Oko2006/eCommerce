e1daf78c5626418b6fa8c32d56dab2f056967a32
Omar Alomari <omaromary445@gmail.com>
2026-04-07 13:57:44 +0300

    New model OrderItem

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -5,3 +5,16 @@ class Order(models.Model):
     user=models.ForeignKey('users.User',related_name='orders',on_delete=models.PROTECT)
     cart=models.OneToOneField('cart.Cart',on_delete=models.PROTECT)
     placed_at=models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10,decimal_places=2)
+
+class OrderItem(models.Model):
+    order=models.ForeignKey('Order',on_delete=models.CASCADE,related_name='order_item')
+    product=models.ForeignKey('products.Product',on_delete=models.PROTECT,related_name='product')
+    product_name=models.CharField(max_length=255)
+    quantity=models.PositiveIntegerField()
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    total_price=models.DecimalField(max_digits=10,decimal_places=2)
+
+    def save(self,*args,**kwargs):
+        self.total_price=self.price*self.quantity
+        super().save(*args,**kwargs)
